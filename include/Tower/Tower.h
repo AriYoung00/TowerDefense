d89d8f0d5227d2795654ab7c3fe49edcb5fc60e5
Ariel Young <arielyoung103@gmail.com>
2016-11-08 18:41:55 -0900

    Redid formatting

--- include/Tower/Tower.h
+++ include/Tower/Tower.h
@@ -22,6 +22,7 @@ namespace Towers {
          * @param location The TilePoint location (tile grid, not pixels) for the tower to be created on
          */
         Tower(TowerType type, TilePoint location);
+
         /**
          * Destructor for the Tower class. Empty as of yet.
          */
@@ -31,10 +32,12 @@ namespace Towers {
          * Tower update method. Called before Tower::render() in each update cycle.
          */
         void update();
+
         /**
          * Tower render method. Is called after Tower::update() in each update cycle.
          */
         void render();
+
         /**
          * Causes a tower to take damage. \param Damage subtracted from \var _health. It is important to note that all of the
          * logic is handled in \ref TowerMan, so it \a is possible for \var _health to have a negative value. Currently
@@ -42,6 +45,7 @@ namespace Towers {
          * @param dmg The amound of damage for the Tower to take
          */
         void takeDamage(int dmg);
+
         /**
          * Adds health to a tower. Param \p health is added to \var _health. If (\var _health + \param health) is greater
          * than the max health for the defined \ref TowerType, then \var _health will be set to the max health for the
@@ -55,11 +59,13 @@ namespace Towers {
          * @return Current Tower health
          */
         int getHealth();
+
         /**
          * Getter function for \var _type
          * @return The \ref TowerType of the Tower object
          */
         TowerType getType();
+
         /**
          * Getter function for the \var _location field
          * @return The TilePoint location of the tower. Is a tile coordinate, not pixel.
@@ -71,10 +77,12 @@ namespace Towers {
          * Tower health
          */
         int _health;
+
         /**
          * TowerType for current tower
          */
         TowerType _type;
+
         /**
          * TilePoint location for current tower
          */
