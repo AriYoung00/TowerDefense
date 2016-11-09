67d6518b172cb731f824587efa7f79eb45f71621
Ariel Young <arielyoung103@gmail.com>
2016-11-08 21:20:35 -0900

    Added compare operator to Location

--- include/Location/TilePoint.h
+++ include/Location/TilePoint.h
@@ -8,16 +8,43 @@
 namespace Location {
     class TilePoint {
     public:
-        TilePoint(int x, int y);
+        /**
+         * Constructor for TilePoint class
+         * @param x The x-coordinate for the TilePoint
+         * @param y The y-coordinate for the TilePoint
+         * @param isTileCoord Whether /p x and /p y refer to a tile coordinate or pixel coordinate. Tile coordinate by
+         *  default. If pixel coordinate, is translated into tile coordinate.
+         */
+        TilePoint(int x, int y, bool isTileCoord = true);
 
-        void getX();
-        void getY();
-        void getPos(); ///Returns {posX, posY}
+        /**
+         * Getter for the x-coordinate held by the TilePoint object.
+         * @return The x-coordinate
+         */
+        int getX();
+
+        /**
+         * Getter for the y-coordinate held by the TilePoint object.
+         * @return The y-coordinate
+         */
+        int getY();
+
+        /**
+         * Returns an array holding both the x- and y-coordinate, in the format {x, y}
+         * @return An array {x, y} of the coordinates held.
+         */
+        int *getPos(); ///Returns {posX, posY}
+
+        /**
+         * Overloaded comparison operator for TilePoint.
+         * @param rhs The TilePoint on the righthand-side of the comparison
+         * @return Whether or not _posX and _posY in both TilePoint objects are equal
+         */
+        bool operator==(TilePoint const &rhs);
 
     private:
         int _posX;
         int _posY;
-
     };
 }
 
--- src/Location/TilePoint.cpp
+++ src/Location/TilePoint.cpp
@@ -1,7 +1,30 @@
 //
 // Created by ariel on 11/7/16.
 //
+#include "Location/TilePoint.h"
 
 namespace Location {
+    TilePoint::TilePoint(int x, int y, bool isTileCoord) {
+        //TODO: Use value of isTileCoord to determine whether or not x and y need to be converted to tile coord. Relies on TileUtil.
 
+        _posX = x;
+        _posY = y;
+    }
+
+    int TilePoint::getX() {
+        return _posX;
+    }
+
+    int TilePoint::getY() {
+        return _posY;
+    }
+
+    int *TilePoint::getPos() {
+        int temp[2] = {_posX, _posY};
+        return temp;
+    }
+
+    bool TilePoint::operator==(TilePoint const &rhs) {
+        return ((_posX == rhs._posX) && (_posY == rhs._posY));
+    }
 }
