c0ea04d21cbbad4fc33ead0da836d30d68c011de
Ariel Young <arielyoung103@gmail.com>
2016-11-14 23:39:16 -0900

    Wrote Monster class

--- src/Monster/Monster.cpp
+++ src/Monster/Monster.cpp
@@ -1,6 +1,47 @@
+#include "Monster/Monster.h"
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
 
 
 namespace Monsters
 {
+    Monster::Monster(/*TilePoint initialLoc, */float speed) : /*_location(initialLoc)*/ _frameClock() {
+        _speed = speed;
 
+        _spriteSheet.loadFromFile("Resources/Textures/MonsterSpriteSheet.png");
+
+        _walkForwardsAnimation.setSpriteSheet(_spriteSheet);
+        _walkForwardsAnimation.addFrame(sf::IntRect(19, 88, 51, 51));
+        _walkForwardsAnimation.addFrame(sf::IntRect(84, 91, 51, 51));
+        _walkForwardsAnimation.addFrame(sf::IntRect(148, 87, 51, 51));
+        _walkForwardsAnimation.addFrame(sf::IntRect(212, 81, 51, 51));
+        _walkForwardsAnimation.addFrame(sf::IntRect(276, 88, 51, 51));
+
+        _sprite.setAnimation(_walkForwardsAnimation);
+        _sprite.setPosition(0, 240);
+    }
+
+    void Monster::update() {
+        //I'll be honest here, I have no idea what this does
+        sf::Time frameTime = _frameClock.restart();
+        //Only one axis of movement for now
+        _sprite.move(_speed, 0);
+        //_location.fromVector(_sprite.getPosition());
+        _sprite.update(frameTime);
+    }
+
+    void Monster::takeDamage(int damage) {
+        _health -= damage;
+    }
+
+    AnimatedSprite &Monster::getSprite() {
+        return _sprite;
+    }
+
+    /*TilePoint Monster::getLocation() {
+        return _location;
+    }*/
 }
--- src/Monster/MonsterMan.cpp
+++ src/Monster/MonsterMan.cpp
@@ -1,6 +1,46 @@
+#include "Monster/MonsterMan.h"
 
+#include <iostream>
 
-namespace Monsters
-{
+using Location::TilePoint;
+using std::cout;
+using std::endl;
 
+namespace Monsters {
+    MonsterMan::MonsterMan(sf::RenderWindow &window) : _window(window), _monsters() {}
+
+    void MonsterMan::update() {
+        if (_spawnClock.getElapsedTime().asSeconds() >= 3) {
+            _spawnClock.restart();
+            createMonster();
+        }
+
+        for (auto &m : _monsters) {
+            m->update();
+        }
+    }
+
+    void MonsterMan::render() {
+        for (auto &m : _monsters) {
+            _window.draw(m->getSprite());
+        }
+    }
+
+    Monster &MonsterMan::getMonstersInRange(const sf::Vector2f &point, double range, Monster &defaultValue) {
+        double distance = 0;
+        for (auto &m : _monsters) {
+            // Sorry about this. It's late, I'm lazy.
+            distance = sqrt(pow(point.x - m->getSprite().getPosition().x, 2) +
+                            pow(point.y - m->getSprite().getPosition().y, 2));
+            if (distance <= range)
+                return *m;
+        }
+
+        return defaultValue;
+    }
+
+    void MonsterMan::createMonster() {
+        // For now, location and speed are set to constant values in Monster, so we just need to fix this later
+        _monsters.push_back(new Monster(0.3));
+    }
 }
