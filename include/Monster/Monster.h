400e8c1d32a9d607af97dfd270ea90c1d32fb0d6
Ariel Young <arielyoung103@gmail.com>
2016-11-14 23:37:29 -0900

    Wrote Monster class

--- include/Monster/Monster.h
+++ include/Monster/Monster.h
@@ -1,4 +1,38 @@
 #ifndef TOWERDEFENSE_MONSTER_H
 #define TOWERDEFENSE_MONSTER_H
 
+#include "Location/TilePoint.h"
+#include "SFMLLib/Animation.hpp"
+#include "SFMLLib/AnimatedSprite.hpp"
+
+using Location::TilePoint;
+
+namespace Monsters {
+    class Monster {
+    public:
+        Monster(/*TilePoint initialLoc, */float speed);
+
+        void update();
+
+        void takeDamage(int damage);
+
+        AnimatedSprite &getSprite();
+
+        TilePoint getLocation();
+
+    private:
+        float _speed;
+        int _health;
+
+        //TilePoint _location;
+
+        sf::Texture _spriteSheet;
+
+        //only animation for now
+        sf::Clock _frameClock;
+        Animation _walkForwardsAnimation;
+        AnimatedSprite _sprite;
+
+    };
+}
 #endif //TOWERDEFENSE_MONSTER_H
--- include/Monster/MonsterMan.h
+++ include/Monster/MonsterMan.h
@@ -5,4 +5,31 @@
 #ifndef TOWERDEFENSE_MONSTERMAN_H
 #define TOWERDEFENSE_MONSTERMAN_H
 
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include "Monster.h"
+
+using std::vector;
+
+namespace Monsters {
+    class MonsterMan {
+    public:
+        MonsterMan(sf::RenderWindow &window);
+
+        Monster &getMonstersInRange(const sf::Vector2f &point, double range, Monster &defaultValue);
+
+        void update();
+
+        void render();
+
+        void createMonster();
+
+    private:
+        vector<Monster *> _monsters;
+
+        sf::Clock _spawnClock;
+        sf::RenderWindow &_window;
+    };
+}
+
 #endif //TOWERDEFENSE_MONSTERMAN_H
