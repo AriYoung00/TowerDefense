289b147173254d940ffbc99d2e98c6a3f2c6f0a9
Collin R McClellan <collinrmcclellan@gmail.com>
2016-11-14 19:10:48 -0900

    wrote constructor and getHealth

--- include/Monster/Monster.h
+++ include/Monster/Monster.h
@@ -1,4 +1,13 @@
 #ifndef TOWERDEFENSE_MONSTER_H
 #define TOWERDEFENSE_MONSTER_H
 
+class Monster
+{
+public:
+	Monster(double health);
+	getHealth();
+private:
+	double health;
+}
+
 #endif //TOWERDEFENSE_MONSTER_H
