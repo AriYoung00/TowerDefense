0004d8b9cba20a1500759229ad0c787177dd547c
Ariel Young <arielyoung103@gmail.com>
2016-11-14 23:40:41 -0900

    Added CamelCase

--- src/Events/EventManager.cpp
+++ src/Events/EventManager.cpp
@@ -1,4 +1,4 @@
-#include "Events/eventmanager.h"
+#include "Events/EventManager.h"
 
 // I'm not sure an event manager is necessary. It doesn't seem to have any purpose other than calling
 // handle() on events it receives, which we could do in the run loop without adding another layer of
